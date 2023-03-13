48bee4a9e272ebb9f00b9cd057b31b384de9da23
ancelinaloysia <ancelin2409@gmail.com>
2023-03-13 11:40:25 +0700

    Tugas Prokom 1

---
+++ Excercise-01.py
@@ -0,0 +1,4 @@
+print("Nama: Ancelin Aloysia Prasetyo")
+print("NIM : I0322013")
+print("     Nama saya Ancelin Aloysia Prasetyo, biasa dipanggil Celin. Saya berasal dari kota yang terkenal akan tembakaunya yaitu Kota Temanggung tepatnya di Brojolan Barat. Rumah saya cukup nyaman karena Temanggung berada di bawah kaki gunung yaitu Sumbing dan Sindoro sehingga udaranya sangat sejuk. Dibelakang rumah saya juga terdapat pemandangan sungai dan sawah yang masih asri. Rumah saya juga sangat dekat dengan beberapa pabrik pembuatan tahu.")
+print("     Saya merupakan mahasiswa Teknik Industri Universitas Sebelas Maret Surakarta. Harapan saya sebagai lulusan Teknik Industri adalah saya dapat membawa nama baik Teknik Industri UNS, membanggakan kedua orang tua, serta sukses dalam menjalankan pekerjaan yang saya inginkan.")
